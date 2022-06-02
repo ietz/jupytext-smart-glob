078837bb89d221e6d7306d02e6d2aa3a0a863802
Tim Pietz <t.pietz@evasys.de>
2022-06-02 12:18:51 +0200

    Take a list of files or glob patterns as input instead of one single directory

---
+++ jupytext_smart_glob/__main__.py
@@ -0,0 +1,4 @@
+from jupytext_smart_glob.cli import main
+
+if __name__ == '__main__':
+    main()
--- jupytext_smart_glob/cli.py
+++ jupytext_smart_glob/cli.py
@@ -1,6 +1,8 @@
 import argparse
+import glob
 import logging
 from pathlib import Path
+from typing import List, Iterable
 
 from jupytext_smart_glob.transform import transform_py_to_ipynb
 
@@ -9,7 +11,20 @@ def main():
     logging.basicConfig()
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('directory', nargs='?', type=Path, default=Path.cwd())
+    parser.add_argument('files', nargs='*')
 
     args = parser.parse_args()
-    transform_py_to_ipynb(files=args.directory)
+    transform_py_to_ipynb(
+        files=get_files_matching_patterns(args.files) if len(args.files) > 0 else Path.cwd().rglob('**/*.py')
+    )
+
+
+def get_files_matching_patterns(patterns: List[str]) -> Iterable[Path]:
+    for pattern in patterns:
+        if "*" in pattern or "?" in pattern:
+            for file_str in glob.glob(pattern, recursive=True):
+                yield Path(file_str)
+        else:
+            file = Path(pattern)
+            if file.is_file():
+                yield file
